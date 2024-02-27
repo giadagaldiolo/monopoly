d939940cb08757facdcadad56f7c46fdd7deb0c8
andre.peitiocampo <andre.peitiocampo@student.supsi.ch>
2024-02-27 19:12:11 +0100

    inputSimboloGiocatore

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Il nostro gruppo è quello che farà il progetto migliore :D");
-        ScannerUtils.inserisciNome(2);
+        ScannerUtils.inputNomeGiocatore(2);
+        ScannerUtils.inputSimboloGiocatore(2); // sono solo di prova
     }
 }
--- src/ScannerUtils.java
+++ src/ScannerUtils.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 public class ScannerUtils {
     private static final Scanner scanner=new Scanner(System.in);
-    private static  int charMax=8;
+    private static final int charMax=8;
 
     static void chiudiScanner(){
         scanner.close();
     }
 
-    public static String inserisciNome(final int numeroGiocatore){
-        String nome="";
+    public static String inputNomeGiocatore(final int numeroGiocatore){
+        String nome;
 
         do {
             System.out.println("Inserisci nome giocatore " + numeroGiocatore);
@@ -18,8 +18,27 @@ public class ScannerUtils {
                 return nome;
 
             }
-            System.out.println("Il nome deve essere massimo di " + charMax +" e non deve essere vuoto");
+            System.out.println("Il nome deve avere massimo " + charMax +" caratteri e non deve essere vuoto");
         }while (true);
 
     }
+
+    public static char inputSimboloGiocatore( final int numeroGiocatore){
+        String inputUtente;
+        do{
+            System.out.println("Inserisci simbolo giocatore " + numeroGiocatore);
+            inputUtente = scanner.nextLine().strip(); // non posso mettere this perche static
+            if ((inputUtente.length()==1) && (!inputUtente.isBlank())){
+                return inputUtente.charAt(0);
+
+            }
+            System.out.println("Inserire soltanto e almeno un carattere");
+        }while (true);
+
+
+    }
+
+
+
+
 }
