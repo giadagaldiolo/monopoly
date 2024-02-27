2132cf102e12be303e0bb198515da7ff369aa034
Linkswitch19 <andrepeiti.123@gmail.com>
2024-02-27 17:14:41 +0100

    Funzione per inserire nome giocatore

--- src/ScannerUtils.java
+++ src/ScannerUtils.java
@@ -2,8 +2,24 @@ import java.util.Scanner;
 
 public class ScannerUtils {
     private static final Scanner scanner=new Scanner(System.in);
+    private static  int charMax=8;
 
     static void chiudiScanner(){
         scanner.close();
+    }
+
+    public static String inserisciNome(final int numeroGiocatore){
+        String nome="";
+
+        do {
+            System.out.println("Inserisci nome giocatore " + numeroGiocatore);
+            nome = scanner.nextLine().strip(); // non posso mettere this perche static
+            if ((nome.length()<=charMax) && (!nome.isBlank())){
+                return nome;
+
+            }
+            System.out.println("Il nome deve essere massimo di " + charMax +" e non deve essere vuoto");
+        }while (true);
+
     }
 }
