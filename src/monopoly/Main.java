8c713fd9e5563e8797565b3c9f11c7707c0f0c66
andre.peitiocampo <andre.peitiocampo@student.supsi.ch>
2024-06-04 02:21:59 +0200

    Breve Commento della personalizzazione

--- src/monopoly/Main.java
+++ src/monopoly/Main.java
@@ -4,4 +4,15 @@ public class Main {
     public static void main(String[] args) {
         new Gioco();
     }
+
+    /*
+      La personalizzazione da noi scelta non aggiunge nulla di nuovo al monopoly, progettato attraverso le serie. Dato che si basa su una breve implementazione grafica
+      tramite javaFX 17 e l'applicazione SceneBuilder che aiuta nella creazione della grafica delle finestre del programma data la sua semplicità.
+      Dato il poco tempo avuto a disposizione per l'extra non si è potuto fare un monopoly totalmente grafico; tuttavia si è puntata s dare ai dadi e all'inizio/fine del programma un UI,
+      lasciando cosi la parte di gameplay e della compravendita a iterazioni tramite terminale.
+      Data la scarsa conoscenza di JavaFx sicuramente non sarà niente di spettacolare, ma abbiamo voluto provarci lo stesso riuscendo ad ottenere qualcosa di almeno funzionale.
+      Per provare la parte visiva basta usare il MainVisuale nel pacchetto monopoly.visuale.
+     */
+
+
 }
